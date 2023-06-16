f9092bcbb61f0e0ea6d5ffb05cba073603ce53fe
Alan <tiltedveggie@outlook.kr>
2023-06-16 13:23:50 -0300

    Add validation pipe to requests

--- src/item.ts
+++ src/item.ts
@@ -1,7 +1,19 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
 export class Item {
+	@IsNumber()
+	@IsOptional()
 	readonly id: number;
+
+	@IsString()
 	readonly name: string;
+
+	@IsNumber()
 	readonly price: number;
+
+	@IsString()
 	readonly description: string;
+
+	@IsString()
 	readonly image: string;
 }
